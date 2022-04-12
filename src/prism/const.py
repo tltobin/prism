a15cd67d585b5e92fe47240c56ee821a8d463313
Taylor Tobin <31015670+tltobin@users.noreply.github.com>
2022-04-12 13:59:03 -0400

    Restructuring Classes and Adding maser_v_theta Class
    -> Renamed base class faraday --> maser.
    -> Added keyword filename to base class maser (formerly faraday), with root of output file name to which the inversion solutions will be saved. (Previously hard-coded as 'FaradayOut'.)
    -> Moved physical constants to separate script const.py
    -> Created new maser base class, _maser_base_, to handle parameter prioritization from call/parameter file/defaults, with option to ignore specific parameters.
        - Class has method, calc_far_coeff, which is referenced by others, but this class requires that W be handed to it explicitly. Methods of the same name in higher level classes provide W from the object attribute of the same name.
        - Class also has lower level methods, _read_par_file_ and _process_key_, to read the parameter file using configparser and find the value for a specific key given a config file section from configparser (if any)  and instructions on the allowed data type and value(s) for the parameter.
    -> Class for single run, maser (formerly faraday) updated to use _maser_base_ for parameter handling in __init__ and its calc_far_coeff method in the maser method of the same name.
    -> Created new method, maser_v_theta, to handle sets of maser objects that vary only in theta.
        - Also uses _maser_base_ class for parameter handling and calc_far_coeff base.
        - maser_v_theta object has most attributes that a maser object would, except for theta. Instead, has thetas, a NumPy array of the values of theta provided, as well as the associated output paths for each, outpaths.
        - Allows theta to be specified in degrees or radians, instead of just radians, through the use of the parameter, units.
        - The maser base class objects for each parameter set/theta are stored in the object attribute masers, a dictionary with the values of theta provided on object initialization (rounded to theta_precision) as keys.
        - The calc_far_coeff, readin, and update_beta methods for the maser_v_theta object update all maser objects in the masers attribute dictionary.

---
+++ src/prism/const.py
@@ -0,0 +1,13 @@
+# Physical constants used by prism
+
+# Elementary charge in C
+e_charge = 1.6021766208e-19
+
+# Vacuum permittivity in F/m
+E0 = 8.854187817620e-12
+
+# Electron mass in kg
+me = 9.10938356e-31
+
+# Speed of light in m/s
+c = 2.99792458e8
